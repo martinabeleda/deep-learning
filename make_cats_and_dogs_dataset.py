b201b84bfa67c5881ce5c989bc4c388bebb31d60
martinabeleda <martin.abeleda@nearmap.com>
2018-08-01 11:03:32 +1000

    added transfer learning notebooks

---
+++ make_cats_and_dogs_dataset.py
@@ -0,0 +1,82 @@
+"""
+Takes the dogs vs cats dataset from Kaggle and creates a small dataset with a train, validate and test split
+https://www.kaggle.com/c/dogs-vs-cats/data
+
+Code from Chollet, F. "Deep Learning with Python"
+"""
+import os, shutil
+
+# path to the directory where the original dataset was uncompressed
+original_dataset_dir = '/Users/martin.abeleda/Development/data/cats-vs-dogs/train')
+
+# directory to store the smaller dataset
+base_dir = os.path.join(os.getcwd(), 'cats_and_dogs_small')
+os.mkdir(base_dir)
+
+train_dir = os.path.join(base_dir, 'train')
+os.mkdir(train_dir)
+validation_dir = os.path.join(base_dir, 'validation')
+os.mkdir(validation_dir)
+test_dir = os.path.join(base_dir, 'test')
+os.mkdir(test_dir)
+
+train_cats_dir = os.path.join(train_dir, 'cats')
+os.mkdir(train_cats_dir)
+
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+os.mkdir(train_dogs_dir)
+
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+os.mkdir(validation_cats_dir)
+
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+os.mkdir(validation_dogs_dir)
+
+test_cats_dir = os.path.join(test_dir, 'cats')
+os.mkdir(test_cats_dir)
+
+test_dogs_dir = os.path.join(test_dir, 'dogs')
+os.mkdir(test_dogs_dir)
+
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(train_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(validation_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
+for fname in fnames:
+
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(test_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(train_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+fnames = ['dog.{}.jpg'.format(i) for i in range(1000, 1500)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(validation_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+
+fnames = ['dog.{}.jpg'.format(i) for i in range(1500, 2000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(test_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+
+print('total training cat images:', len(os.listdir(train_cats_dir)))
+print('total training dog images:', len(os.listdir(train_dogs_dir)))
+print('total validation cat images:', len(os.listdir(validation_cats_dir)))
+print('total validation dog images:', len(os.listdir(validation_dogs_dir)))
+print('total test cat images:', len(os.listdir(test_cats_dir)))
+print('total test dog images:', len(os.listdir(test_dogs_dir)))
